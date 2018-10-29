ddd3691b73525afa10296467bc67de7067068458
João Marcos A. Souza <contato@jmarcossouza.com>
2018-10-29 11:26:28 -0300

    Módulo (carregamento tárdio) de usuário criado; Componente de Login

--- src/app/app.rotas.ts
+++ src/app/app.rotas.ts
@@ -13,5 +13,6 @@ export const ROUTES: Routes = [ //Constante responsável pelas rotas do projeto
     {path: 'sobre', loadChildren: './sobre/sobre.module#SobreModule'}, /*Aqui estou indicando um lazy loading. Só que tem um porém: 
     ...Eu não estou mais carregando o componente, e sim um módulo inteiro. Então eu preciso ir no meu module novo sobre e indicar quem 
     ...vai ser o 'padrão' de carregamento, no caso ele mesmo. (veja sobre.module)*/
+    {path: 'usuario', loadChildren: './usuario/usuario.module#UsuarioModule'},
     {path: '**', component: PagNaoEncontradaComponent} //Os ** são para quando o usuário tentar entrar em uma página não existente
 ]
