e87fbdec7a75a595157f1630abcc37406c2dc682
João Marcos A. Souza <contato@jmarcossouza.com>
2018-10-30 11:43:08 -0300

    Esqueci de uma vírgula

--- src/app/app.rotas.ts
+++ src/app/app.rotas.ts
@@ -15,6 +15,6 @@ export const ROUTES: Routes = [ //Constante responsável pelas rotas do projeto
     ...Eu não estou mais carregando o componente, e sim um módulo inteiro. Então eu preciso ir no meu module novo sobre e indicar quem 
     ...vai ser o 'padrão' de carregamento, no caso ele mesmo. (veja sobre.module)*/
     {path: 'login', loadChildren: './usuario/login/login.module#LoginModule'},
-    {path: 'teste-logado', component: TesteLogadoComponent}
+    {path: 'teste-logado', component: TesteLogadoComponent},
     {path: '**', component: PagNaoEncontradaComponent} //Os ** são para quando o usuário tentar entrar em uma página não existente
 ]
